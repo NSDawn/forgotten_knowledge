9cedd2d3f2a1fa98afa84b9bf1fad707259779ee
NSDawn <tnahsin.airus@gmail.com>
2023-05-25 20:54:23 -0700

    popup box

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,8 @@ function changeTime() {
 
     const LIVING_ROOM_IMG = document.getElementsByClassName("living_room_img")[0];
     LIVING_ROOM_IMG.src = LIVING_ROOM_IMG_PATHS[living_room_img_idx];
+
+    showFact()
 }
 
 
@@ -19,3 +21,20 @@ window.addEventListener('load', function() {
     var loader = document.querySelector('.loader');
     loader.style.display = 'none';
 });
+
+
+const FACT_POPUP_WRAPPER = document.getElementsByClassName("popup_wrapper")[0];
+const FACT_POPUP_CLOSE_BUTTON = document.getElementsByClassName("close_fact_popup")[0];
+
+FACT_POPUP_CLOSE_BUTTON.addEventListener("click", hideFact);
+
+
+function showFact() {
+    FACT_POPUP_WRAPPER.style.display ="block"
+    // handle showing a particular fact... 
+}
+
+function hideFact() {
+    FACT_POPUP_WRAPPER.style.display ="none"
+}
+
